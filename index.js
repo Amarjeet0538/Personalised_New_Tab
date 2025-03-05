dff178e0fd1391ebf7c3dc9ce4f351dc2fc1257e
amarjeet0538 <amarjeet0538@gmail.com>
2025-03-06 02:10:28 +0530

    Add calendera and clock

--- index.js
+++ index.js
@@ -1,20 +1,87 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const searchBox = document.querySelector(".search_input");
-  const searchBtn = document.querySelector(".search_button");
+// Search engine logic
+const searchBox = document.querySelector(".search_input");
 
-  function performSearch() {
-    const searchTerm = searchBox.value.trim();
-    if (searchTerm) {
-      window.location.href = `https://www.google.com/search?q=${encodeURIComponent(
-        searchTerm
-      )}`;
-    }
+function performSearch() {
+  const inputText = searchBox.value.trim();
+
+  if (!inputText) return;
+
+  const prefix = inputText.match(/^([a-zA-Z]\/)(.*)/);
+  if (!prefix) {
+    alert("Invalid format. Use g/ or y/ followed by your search query.");
+    return;
   }
 
-  searchBox.addEventListener("keypress", function (event) {
-    if (event.key === "Enter") {
-      console.log("HEllo");
-      performSearch();
-    }
-  });
+  const searchEngine = prefix[1].toLowerCase();
+  const searchTerm = prefix[2].trim();
+
+  let url = "";
+
+  if (searchEngine === "g/") {
+    url = `https://www.google.com/search?q=${encodeURIComponent(searchTerm)}`;
+  } else if (searchEngine === "y/") {
+    url = `https://www.youtube.com/results?search_query=${encodeURIComponent(
+      searchTerm
+    )}`;
+  } else {
+    alert("Please select a valid search engine.");
+    return;
+  }
+
+  window.location.href = url;
+}
+
+// const searchEngine = document.querySelector(".search_engine");
+
+// function performSearch() {
+//   const searchTerm = searchBox.value.trim();
+
+//   if (!searchTerm) return;
+//   let url = "";
+
+//   if (searchEngine.value === "google") {
+//     url = `https://www.google.com/search?q=${encodeURIComponent(searchTerm)}`;
+//   } else if (searchEngine.value === "yt") {
+//     url = `https://www.youtube.com/results?search_query=${encodeURIComponent(
+//       searchTerm
+//     )}`;
+//   } else {
+//     alert("Please select a valid search engine.");
+//     return;
+//   }
+
+//   window.location.href = url;
+// }
+
+searchBox.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    performSearch();
+  }
 });
+
+//Calendar Logic
+var calendarEl = document.getElementById("calendar");
+
+var calendar = new FullCalendar.Calendar(calendarEl, {
+  initialView: "dayGridMonth",
+});
+
+calendar.render();
+
+//Clock Logic
+
+const clockContent = document.getElementById("digital-clock");
+
+function updateClock() {
+  const now = new Date();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+
+  clockContent.textContent = `${hours}:${minutes}`;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
